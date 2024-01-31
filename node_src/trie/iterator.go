f32e6d1578475fe8455077065857b46b76dc6b5b
hide001 <shubhamswaraj1@gmail.com>
2024-01-31 22:59:56 +0530

    F-2023-0193 patch 4

--- node_src/trie/iterator.go
+++ node_src/trie/iterator.go
@@ -482,11 +482,10 @@ func (it *nodeIterator) push(state *nodeIteratorState, parentIndex *int, path []
 }
 
 func (it *nodeIterator) pop() {
-	last := it.stack[len(it.stack)-1]
-	it.path = it.path[:last.pathlen]
+	parent := it.stack[len(it.stack)-1]
+	it.path = it.path[:parent.pathlen]
 	it.stack[len(it.stack)-1] = nil
 	it.stack = it.stack[:len(it.stack)-1]
-	last.node = nil
 }
 
 func compareNodes(a, b NodeIterator) int {
